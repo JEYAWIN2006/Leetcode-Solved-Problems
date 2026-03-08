0469429296ff7b7656f6e548f5db80755d42b2a8
JEYAWIN GJ <gjeyawin@gmail.com>
2026-03-08 15:23:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.52%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int integerReplacement(int n) {
+        int count=0;
+        if(n==Integer.MAX_VALUE){
+            return 32;
+        }
+        while(n>1){
+            if(n%2==0){
+                n=n/2;
+            }
+            else{
+                if((n+1)%4==0 && (n-1)!=2){
+                    n++;
+                }
+                else{
+                    n--;
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+}
