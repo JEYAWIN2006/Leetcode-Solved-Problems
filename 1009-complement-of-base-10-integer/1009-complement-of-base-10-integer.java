80a3a21385a9f9614e92223618301180eaa09781
JEYAWIN GJ <gjeyawin@gmail.com>
2026-03-11 07:04:38 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.09%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int i=1;
+        if(n==0){
+            return 1;
+        }
+        while(i<n){
+            i=i*2 +1;
+        }
+        return i-n;
+    }
+}
