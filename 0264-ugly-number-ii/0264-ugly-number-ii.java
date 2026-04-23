e45043fd04d32d8ed1be58a77aba8e2cb3d6ae5d
JEYAWIN GJ <gjeyawin@gmail.com>
2026-04-23 11:00:26 +0530

    Time: 2 ms (99.88%), Space: 43.4 MB (66.23%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int nthUglyNumber(int n){
+        int[] ugly=new int[n];
+        ugly[0]=1;
+        int i2=0,i3=0,i5=0;
+        int f2=2,f3=3,f5=5;
+        for(int i=1;i<n;i++){
+            int min=Math.min(Math.min(f2,f3),f5);
+            ugly[i]=min;
+            if(f2==min)
+                f2=2*ugly[++i2];
+            if(f3==min)
+                f3=3*ugly[++i3];
+            if(f5==min)
+                f5=5*ugly[++i5];
+        }
+        return ugly[n-1];
+    }
+}
