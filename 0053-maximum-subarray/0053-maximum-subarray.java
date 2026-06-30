9e433fb9743c382e13701cede646eb8771fcc760
JEYAWIN GJ <gjeyawin@gmail.com>
2026-06-30 21:35:32 +0530

    Time: 1 ms (99.96%), Space: 77.3 MB (31.71%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int m = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int s = 0; s < n; s++) {
+            sum += nums[s];
+            if (sum > m) {
+                m = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return m;
+    }
+}
