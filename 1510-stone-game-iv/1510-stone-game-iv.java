f7e47b053f14e3db2b47bc06db4bb298da267376
JEYAWIN GJ <gjeyawin@gmail.com>
2026-08-10 08:08:17 +0530

    Time: 14 ms (91.38%), Space: 42.7 MB (60.84%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] arr = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (!arr[i - j * j]) {
+                    arr[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return arr[n];
+    }
+}
