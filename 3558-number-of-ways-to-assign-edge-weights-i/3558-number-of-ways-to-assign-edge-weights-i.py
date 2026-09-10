1a8365618778435c6d91de5ae6774ad28d7fe658
JEYAWIN GJ <gjeyawin@gmail.com>
2026-09-10 21:12:12 +0530

    Time: 379 ms (57.29%), Space: 61.1 MB (94.43%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,25 @@
+MOD = 10**9 + 7
+
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        n = len(edges) + 1
+        adj = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        dist = [-1] * (n + 1)
+        dist[1] = 0
+        q = deque([1])
+        max_depth = 0
+        while q:
+            u = q.popleft()
+            for v in adj[u]:
+                if dist[v] == -1:
+                    dist[v] = dist[u] + 1
+                    if dist[v] > max_depth:
+                        max_depth = dist[v]
+                    q.append(v)
+        
+        if max_depth == 0:
+            return 0
+        return pow(2, max_depth - 1, MOD)
